712e3698725b714a02cf0eedc4961f065e77f64c
Ameet Vadhia <ameet01@gmail.com>
2017-10-26 16:32:55 -0700

    implement major changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   attr_reader :password
 
-  validates :username, :session_token, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
-  validates :password, length: {minimum: 6, allow_nil: true}
+  validates :password, length: { minimum: 6, allow_nil: true }
 
   after_initialize :ensure_session_token
 
@@ -27,9 +28,9 @@ class User < ApplicationRecord
   end
 
   def self.find_by_credentials(username, password)
-    @user = User.find_by(username: username)
-    return nil if @user.nil?
-    return @user if @user.is_password?(password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    return user if user.is_password?(password)
     return nil
   end
 
